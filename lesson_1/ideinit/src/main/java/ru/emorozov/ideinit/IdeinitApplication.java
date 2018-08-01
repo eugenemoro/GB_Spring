f7eac52c2b5bc25e7b9f895b52d5eca5210f7d58
eugenemoro <ugn.morozov@gmail.com>
2018-08-01 22:14:25 +0300

    Hello World App created using IDE

---
+++ lesson_1/ideinit/src/main/java/ru/emorozov/ideinit/IdeinitApplication.java
@@ -0,0 +1,21 @@
+package ru.emorozov.ideinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class IdeinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeinitApplication.class, args);
+	}
+
+	@RequestMapping("/**")
+
+	public String helloWorld() {
+		return "Hello world!";
+	}
+}
