a3b490c2b58ba4db0d7b2ea05f2c4d4cf46e3b3f
eugenemoro <ugn.morozov@gmail.com>
2018-08-08 00:48:07 +0300

    Spring Boot + MySQL + Thymeleaf: user CRUD done

--- lesson_2/springbootapp/src/main/java/ru/emorozov/springbootapp/SpringbootappApplication.java
+++ lesson_2/springbootapp/src/main/java/ru/emorozov/springbootapp/SpringbootappApplication.java
@@ -1,7 +1,12 @@
 package ru.emorozov.springbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class SpringbootappApplication {
