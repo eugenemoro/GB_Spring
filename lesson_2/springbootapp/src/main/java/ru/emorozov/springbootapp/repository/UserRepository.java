d1a8f506b98a13787ad9b3b1cf6146cc9a8c099d
eugenemoro <ugn.morozov@gmail.com>
2018-08-08 00:49:12 +0300

    Spring Boot + MySQL + Thymeleaf: user CRUD done

--- lesson_2/springbootapp/src/main/java/ru/emorozov/springbootapp/repository/UserRepository.java
+++ lesson_2/springbootapp/src/main/java/ru/emorozov/springbootapp/repository/UserRepository.java
@@ -1,18 +1,11 @@
 package ru.emorozov.springbootapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.emorozov.springbootapp.entity.User;
 
-import java.util.List;
-
 @Repository
 @Transactional
 public interface UserRepository extends JpaRepository<User, String> {
-
-	@Override
-	@Query("SELECT u FROM User u")
-	List<User> findAll();
 }
