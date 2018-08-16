372b7e62c5dfac779cc145e800711447cce91631
eugenemoro <ugn.morozov@gmail.com>
2018-08-16 21:43:01 +0300

    Spring Boot + MySQL + Thymeleaf + Spring Cloud: modified WebConfig to handle WebViews together with WebService

--- lesson_3/springbootapp/app-spring-boot/src/main/java/ru/emorozov/springbootapp/config/WebConfig.java
+++ lesson_3/springbootapp/app-spring-boot/src/main/java/ru/emorozov/springbootapp/config/WebConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
@@ -15,19 +19,33 @@ import org.springframework.xml.xsd.XsdSchema;
 import ru.emorozov.springbootapp.endpoint.PostEndpoint;
 
 @EnableWs
+@EnableWebMvc
 @Configuration
-public class WebServiceConfig extends WsConfigurerAdapter  {
+public class WebConfig extends WsConfigurerAdapter  {
 
 	@NotNull
 	private static final String WS_MAPPING = "/soap/*";
 
+	@NotNull
+	private static final String WEB_MAPPING = "/*";
+
 	@Bean
 	@SuppressWarnings("unchecked")
-	public ServletRegistrationBean dispatcherServlet(ApplicationContext applicationContext) {
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
 		final MessageDispatcherServlet servlet = new MessageDispatcherServlet();
 		servlet.setApplicationContext(applicationContext);
 		servlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean(servlet, WS_MAPPING);
+	}
+
+	@Bean
+	@SuppressWarnings("unchecked")
+	public ServletRegistrationBean webDispatcherServlet(ApplicationContext applicationContext) {
+		final DispatcherServlet servlet = new DispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setDetectAllViewResolvers(true);
+		servlet.setDetectAllHandlerMappings(true);
+		return new ServletRegistrationBean(servlet, WEB_MAPPING);
 	}
 
 	@Bean(name = "PostService")
@@ -44,5 +62,4 @@ public class WebServiceConfig extends WsConfigurerAdapter  {
 	public XsdSchema postEndpointSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("xsd/postEndpoint.xsd"));
 	}
-
 }
